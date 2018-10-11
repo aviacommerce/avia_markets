1f9e0cde2100cfaa2807f529a192d834e394f6a5
arjun289 <arjun289singh@gmail.com>
2018-10-11 15:04:25 +0530

    Initial Commit

---
+++ lib/avia_markets.ex
@@ -0,0 +1,18 @@
+defmodule AviaMarkets do
+  @moduledoc """
+  Documentation for AviaMarkets.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AviaMarkets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avia_markets_test.exs
@@ -0,0 +1,8 @@
+defmodule AviaMarketsTest do
+  use ExUnit.Case
+  doctest AviaMarkets
+
+  test "greets the world" do
+    assert AviaMarkets.hello() == :world
+  end
+end
